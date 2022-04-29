ee409c1620f4bb9385e690a7bb0132c5be833f8d
dhiman-vansh <80188637+dhiman-vansh@users.noreply.github.com>
2022-04-29 10:48:45 +0530

    Update script.js

--- Module_4/script.js
+++ Module_4/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakult", "Johnny", "Jenny", "Jasonr", "Pauly", "Franky", "Lar", "Paulander", "Laura", "Jimmy"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
